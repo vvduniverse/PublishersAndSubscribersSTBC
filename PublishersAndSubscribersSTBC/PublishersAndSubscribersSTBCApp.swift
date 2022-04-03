c256b3f6ae20e1ce03e222e781c59da07f3cdc61
Vahtee Boo <vahtee.boo@gmail.com>
2022-04-03 16:54:01 +0300

    Initial Commit

---
+++ PublishersAndSubscribersSTBC/PublishersAndSubscribersSTBCApp.swift
@@ -0,0 +1,17 @@
+//
+//  PublishersAndSubscribersSTBCApp.swift
+//  PublishersAndSubscribersSTBC
+//
+//  Created by Vahtee Boo on 03.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct PublishersAndSubscribersSTBCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
